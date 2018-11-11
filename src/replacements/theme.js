f7fa75fec7cc4c2afdfab690b86cb313c5a70fbe
Alexander Teinum <ateinum@gmail.com>
2018-11-12 00:48:10 +0100

    Revert punctuation color, change operator color

--- src/replacements/theme.js
+++ src/replacements/theme.js
@@ -4,7 +4,7 @@ function getCategoryReplacements(colors) {
   return {
     "ColorizedSignatureHelp colors": {
       "HTML Attribute Value": [null, colors.deepOrangeLighter],
-      punctuation: [null, colors.blueGrays[28]],
+      punctuation: [null, colors.blueGrays[24]],
       urlformat: [null, colors.blueLighter]
     },
     "Text Editor Text Marker Items": {
@@ -241,10 +241,10 @@ function getSearchReplaceReplacements(configuration, colors) {
     ["#d0e6f5", blues[36]], // Close and pin icons on active tab
 
     // `<` and `>`
-    ["#808080", blueGrays[28]],
+    ["#808080", blueGrays[24]],
 
     // Operator and HTML operator
-    ["#b4b4b4", blues[32]],
+    ["#b4b4b4", blueGrays[28]],
 
     // Most UI text (menu bar items, tabs, non-selected tabs, console output, Solution Explorer item …)
     ["#f1f1f1", blueGrays[28 + c]],
